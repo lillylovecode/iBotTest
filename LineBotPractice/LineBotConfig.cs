fed8c3c7a87d4791b2a4e5966cad5c798e5b2a3a
Lilly <003631@i-pass.com>
2022-12-15 10:58:39 +0800

    20221215

---
+++ LineBotPractice/LineBotConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace LineBotPractice
+{
+    public class LineBotConfig
+    {
+        public string channelSecret { set; get; }
+        public string accessToken { set; get; }
+    }
+}
---
+++ LineBotPractice/Startup.cs
@@ -0,0 +1,45 @@
+﻿using LineBotPractice;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton<LineBotConfig, LineBotConfig>((s) => new LineBotConfig
+        {
+            channelSecret = Configuration["LineBot:channelSecret"],
+            accessToken = Configuration["LineBot:accessToken"]
+        });
+
+        services.AddHttpContextAccessor();
+        services.AddRazorPages();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "api",
+                pattern: "api/{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapRazorPages();
+        });
+    }
+
+    
+}
