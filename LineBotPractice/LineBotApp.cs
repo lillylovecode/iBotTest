e16ea93fdedfdaaf978831ecabe8c765ec343e1e
Lilly <003631@i-pass.com>
2022-12-15 15:20:50 +0800

    add LIFF.cshtml

--- LineBotPractice/LineBotApp.cs
+++ LineBotPractice/LineBotApp.cs
@@ -9,30 +9,58 @@ public class LineBotApp : WebhookApplication
         _messagingClient = lineMessagingClient;
     }
 
+    /// <summary>
+    /// OnMessageAsync: 接收使用者訊息。
+    /// </summary>
+    /// <param name="ev"></param>
+    /// <returns></returns>
     protected override async Task OnMessageAsync(MessageEvent ev)
     {
-        var result = null as List<ISendMessage>;
+        //var result = null as List<ISendMessage>;
 
-        switch (ev.Message)
+        //switch (ev.Message)
+        //{
+        //    //文字訊息
+        //    case TextEventMessage textMessage:
+        //        {
+        //            //頻道Id
+        //            var channelId = ev.Source.Id;
+        //            //使用者Id
+        //            var userId = ev.Source.UserId;
+
+        //            //回傳 hellow
+        //            result = new List<ISendMessage>
+        //            {
+        //                new TextMessage("hello world~~")
+        //            };
+        //        }
+        //        break;
+        //}
+
+        //if (result != null)
+        //    await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);  //ReplyMessageAsync: 傳訊息給使用者
+
+        var buttonComponent = new ButtonComponent
         {
-            //文字訊息
-            case TextEventMessage textMessage:
+            Style = ButtonStyle.Primary,
+            Action = new UriTemplateAction("LIFF", "line://app/1657738058-89BMdkJ6")
+        };
+
+        var flexMessage = new FlexMessage("LIFF")
+        {
+            Contents = new BubbleContainer
+            {
+                Body = new BoxComponent
                 {
-                    //頻道Id
-                    var channelId = ev.Source.Id;
-                    //使用者Id
-                    var userId = ev.Source.UserId;
-
-                    //回傳 hellow
-                    result = new List<ISendMessage>
+                    Layout = BoxLayout.Vertical,
+                    Contents = new List<IFlexComponent>
                     {
-                        new TextMessage("hello~~")
-                    };
+                     buttonComponent
+                    }
                 }
-                break;
-        }
+            }
+        };
 
-        if (result != null)
-            await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+        await _messagingClient.ReplyMessageAsync(ev.ReplyToken,new List<ISendMessage> { flexMessage });
     }
 }
---
+++ LineBotPractice/Pages/LIFF.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LineBotPractice.Pages
+{
+    public class LIFFModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
